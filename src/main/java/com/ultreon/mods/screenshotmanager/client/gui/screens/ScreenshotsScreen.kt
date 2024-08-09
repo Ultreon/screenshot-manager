f597def8e5364e689ff6866c4b229ae0e56907b5
XyperCode <xypercode@ultreon.dev>
2024-08-09 14:35:40 +0200

    Optimize & comments

--- src/main/java/com/ultreon/mods/screenshotmanager/client/gui/screens/ScreenshotsScreen.kt
+++ src/main/java/com/ultreon/mods/screenshotmanager/client/gui/screens/ScreenshotsScreen.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 package com.ultreon.mods.screenshotmanager.client.gui.screens
 
 import com.badlogic.gdx.Input.Keys
@@ -30,14 +32,10 @@ class ScreenshotsScreen(title: TextObject) : FullscreenRenderScreen(title) {
     var screenshot: Screenshot? = null
         private set
 
-    val files: List<File> get() = files0.toList()
-
     // Getter & setter.
     var index = 0
         private set
     var total = 0
-        private set
-    var loaded = false
         private set
     var isLoading = false
         private set
@@ -49,6 +47,14 @@ class ScreenshotsScreen(title: TextObject) : FullscreenRenderScreen(title) {
         addToolbarItem(ToolbarButton(0, 0, 50, message = CommonTexts.next) { nextShot() })
     }
 
+    /**
+     * Reload the list of screenshots from the "screenshots" directory in the game folder.
+     *
+     * This function is called internally when the class is initialized.
+     * This function does not need to be called manually.
+     *
+     * @return [Unit].
+     */
     private fun reload() {
         val dir = File(QuantumClient.getGameDir().toFile(), "screenshots")
 
@@ -59,26 +65,34 @@ class ScreenshotsScreen(title: TextObject) : FullscreenRenderScreen(title) {
         this.refresh()
     }
 
+    /**
+     * Load the screenshot at the current index.
+     */
     private fun loadScreenshot() {
         val active = AtomicBoolean(true)
         if (this.files0.size == 0) return
         this.files0[this.index].let { file ->
             active.set(true)
             QuantumClient.invoke {
-                val texture: Texture?
-                val data: ScreenshotData
-                val location: Identifier?
+                // Extract the filename, format it and create the identifier
                 val fixedFilename = file.name.lowercase().replace("[^a-z0-9/._]".toRegex(), "_")
                 val id = "screenshot/$fixedFilename"
-                location = ScreenshotManagerMod.id(id)
-                if (!QuantumClient.get().textureManager.isTextureLoaded(location)) {
-                    val dynamicTexture = loadTexture(location, file)
-                    texture = dynamicTexture
-                    data = ScreenshotCache.cache(file, dynamicTexture)
+                val location = ScreenshotManagerMod.id(id)
+
+                // Check if the texture is already loaded
+                val texture = if (QuantumClient.get().textureManager.isTextureLoaded(location)) {
+                    QuantumClient.get().textureManager.getTexture(location)
                 } else {
-                    texture = QuantumClient.get().textureManager.getTexture(location)
-                    data = requireNotNull(ScreenshotCache[file]) { "Screenshot ${file.path} wasn't cached." }
+                    // If not, load the texture and cache it
+                    val dynamicTexture = loadTexture(location, file)
+                    ScreenshotCache.cache(file, dynamicTexture)
+                    dynamicTexture
                 }
+
+                // Retrieve the cached data
+                val data = requireNotNull(ScreenshotCache[file]) { "Screenshot ${file.path} wasn't cached." }
+
+                // Create the screenshot object and update the field
                 val screenshot = Screenshot(file, texture, location, data)
                 this.screenshot = screenshot
             }
@@ -94,85 +108,108 @@ class ScreenshotsScreen(title: TextObject) : FullscreenRenderScreen(title) {
         }
     }
 
-    override fun renderBackground(graphics: Renderer) {
+    /**
+     * Render the background of the screen.
+     */
+    override fun renderBackground(renderer: Renderer) {
         if (client!!.world != null) {
-            renderTransparentBackground(graphics)
+            renderTransparentBackground(renderer)
         } else {
-            renderSolidBackground(graphics)
+            renderSolidBackground(renderer)
         }
 
 
-        graphics.pushMatrix()
-        run {
-            graphics.translate(0f, 0f, 100f)
-            if (this.screenshot != null) {
-                val texture = this.screenshot!!.texture
-                val data = this.screenshot!!.data
-                var location = this.screenshot!!.id
-                if (location == null) {
-                    location = emptyId
-                }
+        renderer.pushMatrix()
+        renderer.translate(0f, 0f, 100f)
+        if (this.screenshot != null) {
+            val texture = this.screenshot!!.texture
+            val data = this.screenshot!!.data
+            var location = this.screenshot!!.id
+            if (location == null) {
+                location = emptyId
+            }
 
-                if (texture != null) {
-                    val imgWidth = data.width
-                    val imgHeight = data.height
-                    val resizer = Resizer(imgWidth.toFloat(), imgHeight.toFloat())
-                    val size = resizer.thumbnail(this.width.toFloat() - 26, this.height.toFloat() - 82)
-                    val centerX = this.width / 2
-                    val centerY = (this.height - 69) / 2 + 48 / 2
-                    val width = size.width.toInt()
-                    val height = size.height.toInt()
+            if (texture != null) {
+                // Calculate the size of the thumbnail
+                val imgWidth = data.width
+                val imgHeight = data.height
+                val resizer = Resizer(imgWidth.toFloat(), imgHeight.toFloat())
+                val size = resizer.thumbnail(this.width.toFloat() - 26f, this.height.toFloat() - 82f)
+                val centerX = this.width / 2
+                val centerY = (this.height - 69) / 2 + 48 / 2
+                val width = size.width.toInt()
+                val height = size.height.toInt()
 
-                    graphics.fill(centerX - width / 2 - 1, centerY - height / 2 - 1, width + 2, height + 2, RgbColor.BLACK)
+                // Draw a black outline around the thumbnail
+                // (this is a little easier to see than the slight border around the image)
+                renderer.fill(
+                    centerX - width / 2 - 1,
+                    centerY - height / 2 - 1,
+                    width + 2,
+                    height + 2,
+                    RgbColor.BLACK
+                )
 
-                    graphics.blit(
-                        location,
-                        centerX - width / 2f,
-                        centerY - height / 2f,
-                        width.toFloat(),
-                        height.toFloat(),
-                        0f,
-                        0f,
-                        imgWidth.toFloat(),
-                        imgHeight.toFloat(),
-                        imgWidth,
-                        imgHeight
-                    )
-                } else {
-                    graphics.blit(location, 0f, 0f, this.width.toFloat(), this.height.toFloat(), 0f, 0f, 16f, 16f, 16, 16)
-                }
-            } else if (this.files0.isNotEmpty() && this.isLoading) {
-                graphics.textCenter(CommonTexts.loading, 2f, this.width / 2, this.height / 2 - 14)
-            } else if (this.files0.isEmpty()) {
-                graphics.textCenter(
-                    CommonTexts.noScreenshots,
-                    2f,
-                    this.width / 2,
-                    this.height / 2
+                // Draw the thumbnail
+                renderer.blit(
+                    location,
+                    centerX - width / 2f, // x position in the top left corner of the thumbnail
+                    centerY - height / 2f, // y position in the top left corner of the thumbnail
+                    width.toFloat(), // width of thumbnail
+                    height.toFloat(), // height of thumbnail
+                    0f, // u position in the top left corner of the image
+                    0f, // v position in the top left corner of the image
+                    imgWidth.toFloat(), // width of image
+                    imgHeight.toFloat(), // height of image
+                    imgWidth, // width of image texture
+                    imgHeight // height of image texture
                 )
             } else {
-                graphics.textCenter(
-                    CommonTexts.errorOccurred,
-                    2f,
-                    this.width / 2,
-                    this.height / 2 - 14
-                )
-                graphics.textCenter(
-                    CommonTexts.invalidScreenshot,
-                    this.width / 2,
-                    this.height / 2
+                renderer.blit(
+                    location,
+                    0f,
+                    0f,
+                    this.width.toFloat(),
+                    this.height.toFloat(),
+                    0f,
+                    0f,
+                    16f,
+                    16f,
+                    16,
+                    16
                 )
             }
+        } else if (this.files0.isNotEmpty() && this.isLoading) {
+            renderer.textCenter(CommonTexts.loading, 2f, this.width / 2, this.height / 2 - 14)
+        } else if (this.files0.isEmpty()) {
+            renderer.textCenter(
+                CommonTexts.noScreenshots,
+                2f,
+                this.width / 2,
+                this.height / 2
+            )
+        } else {
+            renderer.textCenter(
+                CommonTexts.errorOccurred,
+                2f,
+                this.width / 2,
+                this.height / 2 - 14
+            )
+            renderer.textCenter(
+                CommonTexts.invalidScreenshot,
+                this.width / 2,
+                this.height / 2
+            )
         }
-        graphics.popMatrix()
+        renderer.popMatrix()
     }
 
     /**
-     * Load texture file into a resource location.
+     * Load a texture file into a resource location.
      *
      * @param location the resource location to read the texture into.
      * @param file     the file to read.
-     * @return an instance of [DynamicTexture] containing data of the given file.
+     * @return an instance of [Texture] containing data of the given file.
      */
     fun loadTexture(location: Identifier, file: File): Texture {
         try {
@@ -191,6 +228,15 @@ class ScreenshotsScreen(title: TextObject) : FullscreenRenderScreen(title) {
         }
     }
 
+
+    /**
+     * Handles key press events.
+     *
+     * This implementation handles the left and right arrow keys.
+     *
+     * @param keyCode the key code of the key that was pressed.
+     * @return true if the event was handled, false otherwise.
+     */
     override fun keyRelease(keyCode: Int): Boolean {
         if (keyCode == Keys.LEFT) {
             this.prevShot()
@@ -204,6 +250,15 @@ class ScreenshotsScreen(title: TextObject) : FullscreenRenderScreen(title) {
         return super.keyRelease(keyCode)
     }
 
+
+    /**
+     * Handles key press events.
+     *
+     * This implementation handles the left and right arrow keys.
+     *
+     * @param keyCode the key code of the key that was pressed.
+     * @return true if the event was handled, false otherwise.
+     */
     override fun keyPress(keyCode: Int): Boolean {
         if (KeyboardHelper.isHoldingCtrl) {
             if (keyCode == 61 || keyCode == 334) {
@@ -215,6 +270,7 @@ class ScreenshotsScreen(title: TextObject) : FullscreenRenderScreen(title) {
                 return true
             }
         }
+
         return super.keyPress(keyCode)
     }
 
